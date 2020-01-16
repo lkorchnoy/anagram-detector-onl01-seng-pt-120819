dc3774e26f191e18072605b08a6abddccea67351
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-16 07:50:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,16 @@
 class Anagrams
   
+attr_accessor :name
 
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
   
   
-end
+
