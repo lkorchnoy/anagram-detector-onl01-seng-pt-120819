2937c27cf7fdec656e7dc1cdae87db4631669660
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-16 07:43:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagrams
+  
+  ANAGRAM
+  
+  attr_accessor :word
+  
+  
+  def initialize(word)
+    @word = word
+end
+  
+  def match([anagrams])
+    anagrams.detect do |anagram|
+    if ANAGRAM == anagrams
+      anagrams
+    else 
+      @@anagrams = []
+     end
+end
+  
+  
+end
