401728f715f7f9683631c3c8fb52ae9245d982ad
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-16 07:45:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,6 @@
 class Anagrams
   
-  ANAGRAM
-  
-  attr_accessor :word
-  
-  
-  def initialize(word)
-    @word = word
-end
-  
-  def match([anagrams])
-    anagrams.detect do |anagram|
-    if ANAGRAM == anagrams
-      anagrams
-    else 
-      @@anagrams = []
-     end
-end
+
   
   
 end
